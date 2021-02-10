394c3aca2b3a4c98537482ac32429727214d2c88
Denis <dnsy24@gmail.com>
2021-02-09 22:39:54 -0500

    initial commit

---
+++ src/main/java/com/example/springbootmongodbproject/SpringBootMongodbProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootmongodbproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMongodbProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMongodbProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootmongodbproject/SpringBootMongodbProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootmongodbproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMongodbProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
